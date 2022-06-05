170267033bef01416a851c18fa6b4e65cb8d2cdc
Gulnara Tuliakova <gulnaratulyakova@gmail.com>
2022-06-05 20:15:56 +0500

    Add task03 seminar5 HW

---
+++ task03 seminar5 HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу копирования массива
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
+
+}
+
+void PrintArray(int[] printArr)
+{
+    int size1 = printArr.Length;
+    for (int m = 0; m < size1; m++)
+    {
+        Console.Write($"{printArr[m]} ");
+    }
+}
+int[] origineArray = new int[10];
+FillArray(origineArray);
+Console.WriteLine("Массив, заполненный случайными числами:");
+PrintArray(origineArray);
+Console.WriteLine();
+Console.WriteLine("Копия массива:");
+PrintArray(origineArray);
+
+
